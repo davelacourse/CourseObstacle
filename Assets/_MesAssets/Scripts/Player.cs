3d308e2e038e4d6437f90d0ab1b8278b4fded499
ProfDave <dave.lacourse@cegeptr.qc.ca>
2025-09-24 11:31:43 -0400

    cours 2

--- Assets/_MesAssets/Scripts/Player.cs
+++ Assets/_MesAssets/Scripts/Player.cs
@@ -3,22 +3,28 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Vector3 _direction;  // direction dans laquelle le joueur se déplace
-    
+    [SerializeField] private float _vitesseJoueur = 10f;
+    [SerializeField] private float _vitesseRotationJoueur = 1000f;
+
     private void Update()
     {
-        if(Input.GetKey(KeyCode.RightArrow))
-        {
-            _direction = new Vector3(1f, 0f, 0f);
-        }
-        else if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            _direction = new Vector3(-1f, 0f, 0f);
-        }
-        else
-        {
-            _direction= Vector3.zero;
-        }
+        // Récupérer les axes dans le input manager de Unity
+        float directionH = Input.GetAxisRaw("Horizontal");
+        float directionV = Input.GetAxisRaw("Vertical");
 
-        transform.Translate(_direction);
+        _direction = new Vector3(directionH, 0f, directionV);
+
+        _direction.Normalize();  // S'assurer que le vecteur ne dépasse pas 1
+
+        transform.Translate(_direction * Time.deltaTime * _vitesseJoueur, Space.World);
+
+        //Rotation du joueur dans la direction du mouvement
+        if(_direction != Vector3.zero)
+        {
+            Quaternion directionRotation = Quaternion.LookRotation(_direction, Vector3.up);
+            transform.rotation =
+                Quaternion.RotateTowards(transform.rotation, directionRotation,
+                _vitesseRotationJoueur * Time.deltaTime);
+        }
     }
 }
